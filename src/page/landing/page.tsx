2f981e72220fda776b96f2ccb6a2b5396c9d87e8
jongse7 <mmssjj72@naver.com>
2025-05-02 17:00:20 +0900

    feat: preview 추가

--- src/page/landing/page.tsx
+++ src/page/landing/page.tsx
@@ -5,7 +5,8 @@ import { motion } from "framer-motion";
 export function LandingPage() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-white to-slate-100 text-gray-900">
-      <header className="px-6 md:px-12 py-6 flex justify-between items-center">
+      <header className="px-6 md:px-12 py-6 flex flex-col justify-between items-start">
+        <p>나를 바라보는 새로운 시선</p>
         <h1 className="text-2xl font-bold">청동거울</h1>
       </header>
 
@@ -17,18 +18,44 @@ export function LandingPage() {
         viewport={{ once: true }}
       >
         <motion.h2 className="text-4xl md:text-6xl font-bold mb-6">
-          생성형 AI로 개성있는 캐릭터를 생성해봐요
-          <br /> 액티브한 미디어 아트 체험
+          Service Concept.
         </motion.h2>
         <motion.p className="text-lg md:text-xl text-gray-600 mb-2">
-          거울은 단순한 반영의 도구가 아닙니다. 그것은 우리를 비추는 동시에
+          ‘거울’은 단순한 반영의 도구가 아닙니다. 그것은 우리를 비추는 동시에
           새로운 시각을 제시합니다. 본 작품은 다양한 과정을 통해 체험자가 자신의
           모습을 새롭게 경험할 수 있도록 합니다.
+        </motion.p>
+        <div className="h-16" />
+        <motion.p className="text-lg md:text-xl text-gray-600 mb-2">
+          청동거울은 자기 인식과 표현에 대한 새로운 질문이자, 나를 다시 그려보는
+          또 하나의 방식입니다. 이 경험을 통해 나를 바라보는 새로운 시선을
+          얻어보세요.
         </motion.p>
       </motion.section>
 
       <Separator className="my-12 mx-auto w-11/12" />
-
+      <motion.section
+        className="bg-white text-center"
+        initial={{ opacity: 0, y: 30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6 }}
+        viewport={{ once: true }}
+      >
+        <img src="/assets/image/background1.webp" />
+        {/* 가로 스크롤 앱 화면 미리보기 */}
+        <div className="overflow-x-auto">
+          <div className="flex space-x-4 w-max px-2">
+            {[1, 2, 3, 4, 5, 6].map((n) => (
+              <img
+                key={n}
+                src={`/assets/screen/screen${n}.webp`}
+                alt={`앱 화면 ${n}`}
+                className="w-52 h-auto rounded-xl shadow-md transition-transform hover:scale-105"
+              />
+            ))}
+          </div>
+        </div>
+      </motion.section>
       <motion.section
         className="px-6 md:px-24 py-10 bg-white text-center"
         initial={{ opacity: 0, y: 30 }}
@@ -36,9 +63,9 @@ export function LandingPage() {
         transition={{ duration: 0.6 }}
         viewport={{ once: true }}
       >
-        <h2 className="text-2xl md:text-3xl font-bold mb-6">작품 미리보기</h2>
+        <h2 className="text-2xl md:text-3xl font-bold mb-6">Preview.</h2>
         <p className="text-gray-600 mb-8">
-          스마트폰에서 직접 녹화한 실제 체험 영상입니다.
+          체험을 통해 그린 이미지를 AI로 변환된 다양한 스타일의 캐릭터
         </p>
         <div className="w-full max-w-md mx-auto overflow-hidden rounded-2xl shadow-lg">
           <video
