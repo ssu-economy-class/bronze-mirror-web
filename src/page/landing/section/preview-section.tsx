6bedafa3321635157d9c58ded79beef4dd1d54b3
jongse7 <mmssjj72@naver.com>
2025-05-05 15:31:50 +0900

    dsfadfs

--- src/page/landing/section/preview-section.tsx
+++ src/page/landing/section/preview-section.tsx
@@ -1,6 +1,8 @@
 import HorizontalGallery from "@/components/gallery";
+import { useIsMobile } from "@/hooks/use-mobile";
 import { motion } from "framer-motion";
 export default function PreviewSection() {
+  const isMobile = useIsMobile();
   return (
     <motion.section
       className="h-full relative"
@@ -12,7 +14,7 @@ export default function PreviewSection() {
         visible: {},
       }}
     >
-      <div className="relative flex flex-col items-start max-md:ml-4 ml-32 justify-start gap-2 z-10">
+      <div className="relative flex flex-col items-start max-md:ml-8 ml-32 justify-start gap-2 z-10">
         {/* Title */}
         <motion.p
           className="max-md:mt-3 max-md:text-[1.2rem] mt-16 text-4xl font-playfair text-[#0E4838]"
@@ -24,10 +26,17 @@ export default function PreviewSection() {
         >
           Preview.
         </motion.p>
-        <motion.p className="max-md:text-[0.7rem] text-[1rem] text-[#686868] mb-4">
-          체험을 통해 이미지를 그리고 AI로 변환된 다양한 스타일의 캐릭터를
-          만나보세요
-        </motion.p>
+        {isMobile ? (
+          <div className="max-md:text-[0.65rem] text-[1rem] text-[#686868] mb-4">
+            <motion.p>체험을 통해 이미지를 그리고 AI로 변환된</motion.p>
+            <motion.p>다양한 스타일의 캐릭터를 만나보세요</motion.p>
+          </div>
+        ) : (
+          <motion.p className="max-md:text-[0.65rem] text-[1rem] text-[#686868] mb-4">
+            체험을 통해 이미지를 그리고 AI로 변환된 다양한 스타일의 캐릭터를
+            만나보세요
+          </motion.p>
+        )}
       </div>
       <div className="w-screen">
         <HorizontalGallery />
